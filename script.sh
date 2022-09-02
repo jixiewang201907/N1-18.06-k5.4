9489c7c1dd2891a70e87aac0214d60f3e26bb69a
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2022-09-02 19:16:33 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,5 +14,5 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/
 #git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
 # Add luci-app-smartdns
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
-# Add luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
+# Add chinadns-ng
+git clone git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng.git package/chinadns-ng
