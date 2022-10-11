7cbe60cd3d0f43533f48683ff48f0b8901147da9
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2022-10-11 19:30:29 +0800

    Create Imm-script.sh

---
+++ Imm-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd openwrt
+# wireless
+#rm -rf files/etc/config/wireless
+#rm -rf files/etc/modules.d/wireless_enable
+# Add luci-theme-opentomcat
+svn co https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+# Add luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# Add amlogic绠＄悊
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# Add luci-app-passwall
+#git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+# Add luci-app-smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+# Add luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
