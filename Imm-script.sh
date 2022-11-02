b88fa2f65b3b844434577ca11f7b6f2a9c63c4eb
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2022-11-02 18:41:09 +0800

    Update Imm-script.sh

--- Imm-script.sh
+++ Imm-script.sh
@@ -3,6 +3,8 @@ cd openwrt
 # wireless
 #rm -rf files/etc/config/wireless
 #rm -rf files/etc/modules.d/wireless_enable
+# 设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' https://github.com/immortalwrt/immortalwrt/blob/openwrt-21.02/package/base-files/files/etc/shadow
 # Add luci-theme-opentomcat
 svn co https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 # Add luci-theme-argon
@@ -14,4 +16,4 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/
 # Add luci-app-smartdns
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 # Add luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
