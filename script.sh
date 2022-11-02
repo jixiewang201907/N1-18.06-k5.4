e6cc646cde80d4b00e2448caedf08aff393dcd81
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2022-11-02 20:32:17 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ svn co https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-
 # Add luci-theme-argon
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-# Add amlogic绠＄悊
+# Add amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/lean/luci-app-amlogic
 # Add luci-app-passwall
 #git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
