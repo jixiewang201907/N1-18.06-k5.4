069fcb5dfc0e55ab54c81f1d0ad2d7ffced6c261
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2022-11-04 11:04:20 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,3 +16,6 @@ git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lean
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 # Add luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
+
+./scripts/feeds update -a
+./scripts/feeds install -a
