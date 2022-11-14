5893a357d415fde72c3a6aa165359f79f5eeeb12
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2022-11-14 20:23:12 +0800

    Delete Imm-src.sh

--- Imm-src.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd openwrt
-# Add a feed source
-echo 'src-git SSRplus https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git bypass https://github.com/kiddin9/openwrt-bypass' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
