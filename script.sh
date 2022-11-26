63658b523f53f288f2b94f0f15a0f0961651032c
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2022-11-26 18:32:39 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,9 +11,10 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 # Add amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/lean/luci-app-amlogic
 # Add luci-app-passwall
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
+git clone --single-branch -b luci --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
+git clone --single-branch --depth=1 https://github.com/xiaorouji/openwrt-passwall.git  package/lean/passwall-depends
 # Add luci-app-smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 # Add luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 
