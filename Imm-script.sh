89dcddc935af674da9f420f4f212c8fe41654f2d
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-06-08 17:33:20 +0800

    Update Imm-script.sh

--- Imm-script.sh
+++ Imm-script.sh
@@ -3,16 +3,16 @@ cd openwrt
 # wireless
 #rm -rf files/etc/config/wireless
 #rm -rf files/etc/modules.d/wireless_enable
-# Add luci-theme-opentomcat
-svn co https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+# Add luci-theme-kucat
+git clone https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat
 # Add luci-theme-argon
 #rm -rf package/lean/luci-theme-argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 # Add amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 # Add luci-app-passwall
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
+git clone https://github.com/kiddin9/openwrt-packages/luci-app-bypass.git package/luci-app-bypass
 # Add luci-app-smartdns
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 # Add luci-app-adguardhome
