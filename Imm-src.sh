0cf0d8169d452eb5ffce9536a39b0f365a560138
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-10-12 10:12:45 +0800

    Update Imm-src.sh

--- Imm-src.sh
+++ Imm-src.sh
@@ -4,3 +4,4 @@ cd openwrt
 echo 'src-git SSRplus https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+echo 'src-git openwrt-passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
